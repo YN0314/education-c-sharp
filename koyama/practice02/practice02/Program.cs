4e44aa190ec9aed17fbf7ee80d560f34ae12b39e
koya0713 <kovce34996@yahoo.co.jp>
2026-01-04 22:00:03 +0900

    課題2提出

---
+++ koyama/practice02/practice02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 40; i++)
+            { 
+                if (i % 3 == 0 || i.ToString().Contains("3"))
+                {
+                    Console.WriteLine($"{i}アホ");
+                }
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
