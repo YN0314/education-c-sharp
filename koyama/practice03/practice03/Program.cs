98766d07fb4dc0d548392763df7792ead0b16c50
koya0713 <kovce34996@yahoo.co.jp>
2026-01-22 23:31:54 +0900

    要素の入った配列1と配列2を作成、各要素を合計した数を配列3に代入し配列の中身を出力する機能の作成

---
+++ koyama/practice03/practice03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = new int [5] { 1, 2, 3, 4, 5, };
+            int[] second = new int [5] { 6, 7, 8, 9, 10 };
+            int[] total = new int[first.Length];
+
+            for (int i = 0; i < total.Length; i++)
+            {
+                total[i] = first[i] + second[i];
+                Console.WriteLine(total[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
