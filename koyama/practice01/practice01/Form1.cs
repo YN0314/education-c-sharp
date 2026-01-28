55a010821ef6519963f8492dcdb5c9e433fe2a7e
koya0713 <142728767+koya0713@users.noreply.github.com>
2026-01-28 21:04:34 +0900

    課題1提出 (#5)
    * 課題1提出
    
    * -「グー」、「チョキ」、「パー」ボタンを押下し相手の手をTextBoxに表示、勝敗結果をMessageBoxに表示する機能の作成
    -じゃんけんの手をenumで管理するように修正
    -動作確認のキャプチャを追加

---
+++ koyama/practice01/practice01/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApppractice01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum Hand
+        {
+            stone = 0,
+            scissors = 1,
+            paper = 2,
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Hand randomHand = (Hand)rnd.Next(3);
+
+            if(randomHand == Hand.stone)
+            {
+                textBox1.Text = "グー";
+                MessageBox.Show("あいこ", "勝敗");
+            }
+
+            else if (randomHand == Hand.scissors)
+            {
+                textBox1.Text = "チョキ";
+                MessageBox.Show("勝ち", "勝敗");
+            }
+            else if (randomHand == Hand.paper)
+            {
+                textBox1.Text = "パー";
+                MessageBox.Show("負け", "勝敗");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Hand randomHand = (Hand)rnd.Next(3);
+
+            if (randomHand == Hand.scissors)
+            {
+                textBox1.Text = "チョキ";
+                MessageBox.Show("あいこ", "勝敗");
+            }
+
+            else if (randomHand == Hand.paper)
+            {
+                textBox1.Text = "パー";
+                MessageBox.Show("勝ち", "勝敗");
+            }
+            else if (randomHand == Hand.stone)
+            {
+                textBox1.Text = "グー";
+                MessageBox.Show("負け", "勝敗");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Hand randomHand = (Hand)rnd.Next(3);
+
+            if (randomHand == Hand.paper)
+            {
+                textBox1.Text = "パー";
+                MessageBox.Show("あいこ", "勝敗");
+            }
+
+            else if (randomHand == Hand.stone)
+            {
+                textBox1.Text = "グー";
+                MessageBox.Show("勝ち", "勝敗");
+            }
+            else if (randomHand == Hand.scissors)
+            {
+                textBox1.Text = "チョキ";
+                MessageBox.Show("負け", "勝敗");
+            }
+        }
+    }
+}
