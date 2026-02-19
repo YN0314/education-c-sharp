95dc68a30456c4bccb5c7f9c6906878b57b46048
koya0713 <kovce34996@yahoo.co.jp>
2026-02-20 01:38:47 +0900

    first/secondの値も表示させワンラインで出力するように修正

--- koyama/practice03/practice03/Program.cs
+++ koyama/practice03/practice03/Program.cs
@@ -14,12 +14,24 @@ namespace practice03
             int[] second = new int [5] { 6, 7, 8, 9, 10 };
             int[] total = new int[first.Length];
 
-            for (int i = 0; i < total.Length; i++)
+            for (int i = 0; i < first.Length; i++)
             {
                 total[i] = first[i] + second[i];
-                Console.WriteLine(total[i]);
             }
+
+            PrintArray(first);
+            PrintArray(second);
+            PrintArray(total);
+
             Console.ReadLine();
+        }
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
