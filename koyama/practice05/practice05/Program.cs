57d5ea4a145895ca2ed254c2cd9045edd7e045bf
s.ochiai <ochiai.shotaro@brave-heart.co.jp>
2026-02-24 16:46:54 +0900

    課題5提出

---
+++ koyama/practice05/practice05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+
+            c.pi = 3.14;
+            c.r = 4;
+            c.Circmference();
+            c.Area();
+
+            Console.ReadLine();
+
+        }
+    }
+}
