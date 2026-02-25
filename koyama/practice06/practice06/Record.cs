13855156ba0f00a61a66bd6fd5be55b5514c2422
s.ochiai <ochiai.shotaro@brave-heart.co.jp>
2026-02-25 13:53:15 +0900

    課題6提出

---
+++ koyama/practice06/practice06/Record.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice06
+{
+    internal class Record
+    {
+        public string name;
+        public int englishscore;
+        public int japanesescore;
+        public int mathscore;
+
+        public Record(string name, int englishscore, int japanesescore, int mathscore)
+        {
+            this.name = name;
+            this.englishscore = englishscore;
+            this.japanesescore = japanesescore;
+            this.mathscore = mathscore;
+        }
+
+        public int Sum()
+        {
+            return englishscore + japanesescore + mathscore;
+        }
+
+        public int Avg()
+        {
+            return Sum() / 3;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"名前：{name}");
+            Console.WriteLine($"英語：{englishscore}");
+            Console.WriteLine($"国語：{japanesescore}");
+            Console.WriteLine($"数学：{mathscore}");
+            Console.WriteLine($"合計点:{Sum()}");
+            Console.WriteLine($"平均点：{Avg()}");
+        }
+    }
+}
