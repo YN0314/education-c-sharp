826ca90f2d343e8e307ca0fea997515857da14a8
koya0713 <142728767+koya0713@users.noreply.github.com>
2026-03-24 09:10:51 +0900

    課題2提出 (#6)
    * 課題2提出
    
    * 条件外の場合も数字が出力されるように修正、エビデンスファイルの追加
    
    ---------
    
    Co-authored-by: s.ochiai <ochiai.shotaro@brave-heart.co.jp>

---
+++ koyama/practice02/practice02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 40; i++)
+            { 
+                if (i % 3 == 0 || i.ToString().Contains("3"))
+                {
+                    Console.WriteLine($"{i}アホ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
