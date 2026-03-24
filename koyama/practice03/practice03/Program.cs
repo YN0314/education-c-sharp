1a2456019590bd3c048351625254ee3443671702
koya0713 <142728767+koya0713@users.noreply.github.com>
2026-03-24 09:14:28 +0900

    課題3提出 (#7)
    * 課題1提出
    
    * -「グー」、「チョキ」、「パー」ボタンを押下し相手の手をTextBoxに表示、勝敗結果をMessageBoxに表示する機能の作成
    -じゃんけんの手をenumで管理するように修正
    -動作確認のキャプチャを追加
    
    * 要素の入った配列1と配列2を作成、各要素を合計した数を配列3に代入し配列の中身を出力する機能の作成
    
    * first/secondの値も表示させワンラインで出力するように修正

---
+++ koyama/practice03/practice03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = new int [5] { 1, 2, 3, 4, 5, };
+            int[] second = new int [5] { 6, 7, 8, 9, 10 };
+            int[] total = new int[first.Length];
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                total[i] = first[i] + second[i];
+            }
+
+            PrintArray(first);
+            PrintArray(second);
+            PrintArray(total);
+
+            Console.ReadLine();
+        }
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
