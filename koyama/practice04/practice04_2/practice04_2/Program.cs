71298b8f378743c07ea3ea72c2ac8663009b4e83
koya0713 <142728767+koya0713@users.noreply.github.com>
2026-03-24 09:20:37 +0900

    課題4提出 (#8)
    * 課題4提出
    
    * int型の20行20列の2次元配列を作成、i行j列の配列要素に「i+jの値が6で割り切れる」または「i-jの値が6で割り切れる」場合に1を代入そうでない場合に0を代入、配列の中身が1の場合は□を出力、0の場合は■を出力する機能の作成
    
    ---------
    
    Co-authored-by: s.ochiai <ochiai.shotaro@brave-heart.co.jp>

---
+++ koyama/practice04/practice04_2/practice04_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice04_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] box = new int[20, 20];
+            for (int i = 0; i < box.GetLength(0); i++)
+            {
+                for (int j = 0; j < box.GetLength(1); j++)
+                {
+                    int addition = i + j;
+                    int subtraction = i - j;
+
+                    if (addition % 6 == 0 || subtraction % 6 == 0)
+                    {
+                        box[i, j] = 1;
+                    }
+                    else 
+                    {
+                        box[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 0;i < box.GetLength(0); i++)
+            {
+                for (int j = 0;j < box.GetLength(1); j++)
+                {
+                    if (box[i, j] == 0)
+                    {
+                        Console.Write("■");
+                    }
+
+                    else if (box[i, j] == 1)
+                    {
+                        Console.Write("□");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
